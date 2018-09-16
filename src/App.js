57cd452a70048f75fcee3f9269b7ed6246ba3608
Nytheneals <nythes@gmail.com>
2018-09-16 14:12:33 -0400

    Added Apollo Client and Provider

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+// WE IMPORT APOLLO CLIENT FROM APOLLO BOOST
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import gql from 'graphql-tag';
+import logo from './logo.svg';
+import './App.css';
+
+// CONNECTING TO THE APOLLO SERVER USING APOLLO BOOST THRU APOLLO CLIENT
+const client = new ApolloClient({
+  uri: 'https://w5xlvm3vzz.lp.gql.zone/graphql',
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
